9c98e4b3d89cf3887e5c7a988fa834b45d8b1f2c
JofreMontes <Alexissaravia159@gmail.com>
2025-05-09 15:35:10 -0500

    solo documentacion

--- Web_Reserva_Vet/Models/Usuario.cs
+++ Web_Reserva_Vet/Models/Usuario.cs
@@ -2,6 +2,8 @@
 {
     public class Usuario
     {
+        //esta clase es de ejemplo para probar
+        //el inserción de tablas en la base de datos
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Direccion { get; set; }
