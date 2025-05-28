fcb1a4bd9745f2eec7b6ab234c0f0f23bba55029
JofreMontes <Alexissaravia159@gmail.com>
2025-05-28 18:37:31 -0500

    Primera-union-web

---
+++ Web_Reserva_Vet/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult IndexLogin()
+    {
+        return View();
+    }
+
+    public IActionResult ServiciosLogin()
+    {
+        return View();
+    }
+    
+    public IActionResult EmpleadosLogin()
+    {
+        return View();
+    }
+    
+    public IActionResult ContactoLogin()
+    {
+        return View();
+    }
+    public IActionResult CitasLogin()
+    {
+        return View();
+    }
+
+}
+
+
